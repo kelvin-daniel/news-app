220a12f6042496fcc0448c44500caa0a2d48a13d
kelvin-daniel <kaymutor@gmail.com>
2020-09-10 12:01:46 +0300

    set up configuration

--- app/views.py
+++ app/views.py
@@ -9,4 +9,12 @@ def index():
     '''
     message = 'Home'
     return render_template('index.html', message = message)
+#dynamic route
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
 
