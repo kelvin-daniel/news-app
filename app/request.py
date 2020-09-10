2a5b1723ac44ade1b90bc2bff696e1766c5942c1
kelvin-daniel <kaymutor@gmail.com>
2020-09-10 18:43:28 +0300

    add get articles functions

--- app/request.py
+++ app/request.py
@@ -1,8 +1,8 @@
 from app import app
 import urllib.request,json
-from .models import news
+from .models import source
 
-News = news.News
+Sources = source.Sources
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 # Getting the base url
@@ -14,15 +14,15 @@ def get_sources(category):
     '''
     get_sources_url = base_url.format(category,api_key)
 
-    with urllib.request.urlopen(get_news_url) as url:
+    with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
         get_sources_response = json.loads(get_sources_data)
 
-        news_results = None
+        sources_results = None
 
         if get_sources_response['sources']:
-            news_results_list = get_sources_response['sources']
-            sources_results = process_sources (news_sources_list)
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources (sources_results_list)
     return sources_results
 
 def process_sources(sources_list):
@@ -33,17 +33,47 @@ def process_sources(sources_list):
     returns: news results
     '''
     sources_results= []
-    for news_item in sources_list:
-        id = sources_item.get('id')
-        name = sources_item.get('name')
-        description = sources_item.get('description')
-        url = sources_item.get('url')
-        category = sources_item.get('category')
-        language = sources_item.get('language')
-        country = sources_item.get('country')
-
-        if name:
-            sources_object = News(id,name,description,url,category,language,country)
-            sources_results.append(sources_object)
-
+    for source_item in sources_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+        sources_object = Sources(id,name,description,url,category,language,country)
+        sources_results.append(sources_object)
     return sources_results
+def get_articles(id):
+	'''
+	Function that processes the articles and returns a list of articles objects
+	'''
+	get_articles_url = articles_url.format(id,api_key)
+
+	with urllib.request.urlopen(get_articles_url) as url:
+		articles_results = json.loads(url.read())
+
+		articles_object = None
+		if articles_results['articles']:
+			articles_object = process_articles(articles_results['articles'])
+	return articles_object
+
+def process_articles(articles_list):
+	'''
+    Function that processes the news articles results and returns a list of objects
+	'''
+	articles_object = []
+	for article_item in articles_list:
+		id = article_item.get('id')
+		author = article_item.get('author')
+		title = article_item.get('title')
+		description = article_item.get('description')
+		url = article_item.get('url')
+		image = article_item.get('urlToImage')
+		date = article_item.get('publishedAt')
+
+		if image:
+			articles_result = Articles(id,author,title,description,url,image,date)
+			articles_object.append(articles_result)	
+
+	return articles_object
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_news
+from .request import get_sources, get_articles
 
 #views
 @app.route('/')
@@ -9,7 +9,7 @@ def index():
     function that returns the index page and its data
     '''
     # Getting tech news
-    news = get_news('technology')
+    news = get_sources('technology')
     title = 'Home- news hub'
     return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
 #dynamic route
