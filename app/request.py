4b53cd7bff6bc48096b1bccd20f706b484a7acd4
kelvin-daniel <kaymutor@gmail.com>
2020-09-10 14:21:33 +0300

    add function to process news data

--- app/request.py
+++ app/request.py
@@ -24,3 +24,26 @@ def get_news(sources):
             news_results_list = get_news_response['results']
             news_results = process_results(news_results_list)
     return news_results
+
+def process_results(news_list):
+    '''
+    function that processes the news results and transform them to a listof objects
+    args:
+        news_list: a list of dictionaries that contain news details
+    returns: news results
+    '''
+    news_results= []
+    for news_item in news_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
+        language = news_item.get('language')
+        country = news_item.get('country')
+
+        if name:
+            movie_object = Movie(id,name,description,url,category,language,country)
+            news_results.append(news_object)
+
+    return news_results
