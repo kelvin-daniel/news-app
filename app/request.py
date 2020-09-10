abd005bc20c641ce8b9b9be658ee14971403188e
kelvin-daniel <kaymutor@gmail.com>
2020-09-10 13:09:28 +0300

    fetch base url and api key from config

--- app/request.py
+++ app/request.py
@@ -1,4 +1,9 @@
 from app import app
+import urllib.request,json
+from .models import movie
 
+News = news.News
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+# Getting the base url
+base_url = app.config['NEWS_API_BASE_URL']
