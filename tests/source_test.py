1a06efa124c95f409ddd0356ef3732183405bbb4
kelvin-daniel <kaymutor@gmail.com>
2020-09-11 13:20:46 +0300

    remove test run cmd

--- tests/source_test.py
+++ tests/source_test.py
@@ -1,6 +1,5 @@
 import unittest
-from models import source
-Sources = source.Sources
+from models import Sources,
 
 class NewsTest(unittest.TestCase):
     '''
@@ -13,5 +12,14 @@ class NewsTest(unittest.TestCase):
         self.new_source = Sources(1234,'ABC News','description','"http://www.abc.net.au/news"','general','en','au')
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Sources))
-if __name__ == '__main__':
-    unittest.main()
+class ArticlesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Articles('CNN','Peter Polle','The tech scene in Africa-Is it the next big thing?','A look at various tech hubs in Africa and the impact they have on the worlds economy','techie.com','techie.com/7643t94.jpg','2018-04-11T07:57:16Z')
+
