225096f23c1ec90f0ba3b15c14bb41319c389a78
kelvin-daniel <kaymutor@gmail.com>
2020-09-13 20:30:35 +0300

    change articles layout

--- manage.py
+++ manage.py
@@ -13,5 +13,5 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-if __name__=='__main__':
+if __name__== '__main__':
     manager.run()
